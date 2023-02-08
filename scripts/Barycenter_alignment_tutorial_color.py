5a0cb5ce355c20bc29cb31a10c682817f8004039
Masafumi Oizumi <c-oizumi@g.ecc.u-tokyo.ac.jp>
2023-02-08 16:40:06 +0900

    corrected color tutorial

--- scripts/Barycenter_alignment_tutorial_color.py
+++ scripts/Barycenter_alignment_tutorial_color.py
@@ -20,7 +20,6 @@ from src.GW_alignment import GW_alignment
 # load data
 # similarity matrix
 n_colors = 93
-n_trials = 20
 n_sub = 5
 data_path = f"../data/num_groups_5_seed_0_fill_val_3.5.pickle"
 with open(data_path, "rb") as f:
@@ -32,8 +31,15 @@ sim_mat_list = data["group_ave_mat"]
 file_path = "../data/color_dict.csv"
 data_color = pd.read_csv(file_path)
 color_label = data_color.columns.values
-# col_dict = make_colour_idx_dict(color_codes)
 
+#%%
+# Show simmilarity matrices
+fig = plt.figure(figsize=(25, 5))
+for i in range(n_sub):
+    ax = fig.add_subplot(1, 5, i + 1)
+    sns.heatmap(sim_mat_list[i], square=True, ax = ax)
+    plt.title("Subject {}".format(i + 1))
+plt.show()
 #%%
 # MDS
 seed = 0 # random seed
@@ -44,7 +50,7 @@ for sim_mat in sim_mat_list:
     embedding_list.append(X)
 #%%
 # visualization of embeddigs for each subject
-fig = plt.figure(figsize=(20, 100))
+fig = plt.figure(figsize=(25, 5))
 for i in range(n_sub):
     ax = fig.add_subplot(1, 5, i + 1, projection='3d')
     coords_i = embedding_list[i]
@@ -54,31 +60,17 @@ for i in range(n_sub):
            marker="o", color=color_label, s=10)
     plt.title(title, fontsize=14)
 plt.show()
-
-#%%
-sim_mat_list = np.zeros((n_sub, n_colors, n_colors))
-for i in range(n_sub):
-    sim_mat_list[i] = distance.cdist(embedding_list[i], embedding_list[i], 'cosine')
-    
-# Show simmilarity matrices
-fig = plt.figure(figsize=(20, 20))
-for i in range(n_sub):
-    ax = fig.add_subplot(6, 6, i + 1)
-    sns.heatmap(sim_mat_list[i], square=True, ax = ax)
-    plt.title("Subject {}".format(i + 1))
-plt.show()
-
 #%%
 # Set parameters
 n_init_plan = 5 #  the number of initial plans
-n_trials = 10 # number of epsilons
-epsilons = np.linspace(0.02, 0.14, n_trials)
-epsilon_range = [1e-4, 1e-2] # search range of epsilon
+n_epsilons = 10 # number of epsilons
+epsilons = np.linspace(0.02, 0.14, n_epsilons)
+epsilon_range = [0.02, 0.14] # search range of epsilon
 n_jobs = 10 # the number of cores
 init_diag = True 
 
-DATABASE_URL = 'mysql+pymysql://root@localhost/takeda_GWOT'
-name_list = [f"Subject{i + 1}" for i in range(n_sub)]
+DATABASE_URL = 'mysql+pymysql://root@localhost/oizumi'
+name_list = [f"Group {i + 1}" for i in range(n_sub)]
 
 #%%
 # Set the instance
@@ -86,29 +78,28 @@ barycenter = Barycenter_alignment(n_sub, embedding_list, pivot = 0, DATABASE_URL
 
 # GW alignment to the pivot
 # With optuna
-# Pi_list_before = barycenter.gw_alignment_to_pivot(optuna = True, 
-#                                                  n_init_plan = n_init_plan, 
-#                                                  epsilon_range = epsilon_range, 
-#                                                  n_trials = n_trials, 
-#                                                  n_jobs = n_jobs, 
-#                                                  init_diag = init_diag)
-
-# Without using optuna
-Pi_list_before = barycenter.gw_alignment_to_pivot(optuna = False, 
+Pi_list_before = barycenter.gw_alignment_to_pivot(optuna = True, 
                                                   n_init_plan = n_init_plan, 
-                                                  epsilons = epsilons, 
-                                                  n_trials = n_trials, 
+                                                  epsilon_range = epsilon_range, 
+                                                  n_trials = n_epsilons, 
                                                   n_jobs = n_jobs, 
                                                   init_diag = init_diag)
 
+# Without using optuna
+# Pi_list_before = barycenter.gw_alignment_to_pivot(optuna = False, 
+#                                                   n_init_plan = n_init_plan, 
+#                                                   epsilons = epsilons, 
+#                                                   n_trials = n_epsilons, 
+#                                                   n_jobs = n_jobs, 
+#                                                   init_diag = init_diag)
+
 #%%
 # Barycenter alignment
-Pi_list_after = barycenter.main_compute(Pi_list_before, max_iter = 10)
+Pi_list_after = barycenter.main_compute(Pi_list_before, max_iter = 30)
 barycenter.calc_correct_rate_for_pairs(Pi_list_before, Pi_list_after)
 
 #%%
 # Plot
 markers_list = ["o", "v", "1","2","3","4","<",">","^","8","s","p","P","h","H","+","x","X","D","d","|","_",0,1,2,3,4,5,6,7,8]
 barycenter.plot_embeddings(markers_list, color_label)
-
-#%%
+# %%
