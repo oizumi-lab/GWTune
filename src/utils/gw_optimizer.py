dd2b1bd91f2fc6d2e236adef33bafe974e4b2ef1
AbebaYz <k.abe.i.fishstory@gmail.com>
2023-04-04 18:14:47 +0900

    ・gw_optimizer.pyのload＿studyを少し変更 ・tutorial.pyのpath設定を変更

--- src/utils/gw_optimizer.py
+++ src/utils/gw_optimizer.py
@@ -67,13 +67,13 @@ class RunOptuna():
         self.to_types = to_types
         self.storage = storage
         self.filename = filename
-        
+
         # optunaにおける各種設定
         self.sampler_name = sampler_name
         self.pruner_name = pruner_name
         self.pruner_params = pruner_params
         self.sql_name = sql_name
-    
+
         # optuna.studyに与えるパラメータ
         self.n_jobs = n_jobs
         self.num_trial = num_trial
@@ -82,7 +82,7 @@ class RunOptuna():
         # MedianPruner
         self.n_startup_trials = 5
         self.n_warmup_steps = 5
-        
+
         # HyperbandPruner
         self.min_resource = 5
         self.reduction_factor = 2
@@ -116,53 +116,58 @@ class RunOptuna():
                 raise ValueError("If you don't want to delete study, use 'delete_study = False'.")
             else:
                 print("Invalid input. Please enter again.")
-    
+
+    def create_study(self, seed = 42):
+        study = optuna.create_study(direction = "minimize",
+                                    study_name = self.filename,
+                                    sampler = self.choose_sampler(seed = seed),
+                                    pruner = self.choose_pruner(),
+                                    storage = self.storage,
+                                    load_if_exists = True)
+        return study
+
+
     def load_study(self, seed = 42):
         """
         2023.4.3 佐々木
         studyファイルの作成を行う関数。
-        
+
         Returns:
             _type_: _description_
         """
         db_file_path = self.save_path + '/' + self.filename + '.db'
-        
+
         if os.path.exists(db_file_path):
-            study = optuna.load_study(study_name = self.filename, 
+            study = optuna.load_study(study_name = self.filename,
                                       sampler = self.choose_sampler(seed = seed),
                                       pruner = self.choose_pruner(),
                                       storage = self.storage)
 
         else:
-            study = optuna.create_study(direction = "minimize",
-                                        study_name = self.filename,
-                                        sampler = self.choose_sampler(seed = seed),
-                                        pruner = self.choose_pruner(),
-                                        storage = self.storage,
-                                        load_if_exists = True)
+            raise ValueError('This db does not exist.')
         return study
-        
-        
+
+
 
     def run_study(self, objective, gpu_board = 'cuda:0', forced_run = True):
         """
         2023.3.29 佐々木
-        
+
         objective = functools.partial(dataset, init_plans_list = init_mat_types, eps_list = self.eps_list)
         """
         if self.delete_study:
             self._confirm_delete()
-        
+
         if forced_run:
-            self.load_study() #dbファイルがない場合、ここでloadをさせないとmulti_runが正しく動かなくなってしまう。
-            
+            self.create_study() #dbファイルがない場合、ここでloadをさせないとmulti_runが正しく動かなくなってしまう。
+
             def multi_run(objective, seed, num_trials, device):
                 tt = functools.partial(objective, device = device)
                 study = self.load_study(seed = seed)
                 study.optimize(tt, n_trials = num_trials, n_jobs = 1)
 
             seed = 42
-            
+
             with ThreadPoolExecutor(self.n_jobs) as pool:
                 for i in range(self.n_jobs):
                     if gpu_board == 'multi':
@@ -171,7 +176,7 @@ class RunOptuna():
                         device = gpu_board
                     elif gpu_board == 'cpu':
                         device = 'cpu'
-                    
+
                     pool.submit(multi_run, objective, seed + i, self.num_trial // self.n_jobs, device)
 
         study = self.load_study()
