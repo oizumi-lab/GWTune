b2c2f7f071a499f979397d2a63bf2c55b9298d84
MSA8D8 <demo.abc2@gmail.com>
2023-04-12 18:14:57 +0900

    バグを発見

--- experiment/optuna/optuna_test_multiprocessing.py
+++ experiment/optuna/optuna_test_multiprocessing.py
@@ -4,20 +4,21 @@ import torch
 import torch.multiprocessing as mp
 import numpy as np
 torch.manual_seed(42)
-
+from concurrent.futures import ThreadPoolExecutor
 
 print('---------')
 
 def objective(trial, test_arr):
-    x = trial.suggest_float("x", 10, 100)
+    x = trial.suggest_float("x", -10, 10)
     return sum(test_arr * x)
  
 study = optuna.create_study(study_name="my_study", storage = "sqlite:///cuda_test.db", load_if_exists = True)
 
 
 def multi_run(seed, i, n_trials):
-    # device = 'cuda:0'#+str(i)
-    test_arr = torch.randn(10).to('cpu')
+    device = 'cuda:0'#+str(i)
+    test_arr = torch.randn(10).to(device)
+    print(test_arr)
     # sampler = optuna.samplers.RandomSampler(seed = seed + i)
     sampler = optuna.samplers.GridSampler(search_space, seed = seed + i)
     loaded_study = optuna.load_study(sampler = sampler, study_name="my_study", storage="sqlite:///cuda_test.db")
@@ -30,7 +31,9 @@ n_trials = 20
 seed = 42
 
 
-search_space = {'x':np.logspace(1, 2, num = n_trials)}
+search_space = {'x':np.logspace(0, 1, num = n_trials)}
+
+# %%
 # プロセスを生成する
 processes = []
 for i in range(n_jobs):
@@ -44,4 +47,9 @@ for p in processes:
 # # プロセスが終了するのを待つ
 for p in processes:
     p.join()
+# %%
+with ThreadPoolExecutor(n_jobs) as pool:
+    for i in range(n_jobs):
+        pool.submit(multi_run, seed, i, n_trials // n_jobs)
+
 # %%
