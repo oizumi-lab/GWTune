aa54add2ef2225a51268cf355f0c31e567219bea
AbebaYz <k.abe.i.fishstory@gmail.com>
2023-04-24 14:11:51 +0900

    minor change

--- src/utils/gw_optimizer.py
+++ src/utils/gw_optimizer.py
@@ -161,23 +161,10 @@ class RunOptuna:
             else:
                 print("Invalid input. Please enter again.")
 
-<<<<<<< HEAD
-    def create_study(self, seed=42):
-        study = optuna.create_study(
-            direction="minimize",
-            study_name=self.filename,
-            sampler=self.choose_sampler(seed=seed),
-            pruner=self.choose_pruner(),
-            storage=self.storage,
-            load_if_exists=True,
-        )
-=======
     def create_study(self):
-        study = optuna.create_study(direction = "minimize",
-                                    study_name = self.filename,
-                                    storage = self.storage,
-                                    load_if_exists = True)
->>>>>>> 732d91eba67f6d58e7c5ccb6395b4259c14f060d
+        study = optuna.create_study(
+            direction="minimize", study_name=self.filename, storage=self.storage, load_if_exists=True
+        )
         return study
 
     def load_study(self, seed=42):
@@ -188,126 +175,83 @@ class RunOptuna:
         Returns:
             _type_: _description_
         """
-        if self.sql_name == "sqlite":
-            db_file_path = self.save_path + "/" + self.filename + ".db"
-            if not os.path.exists(db_file_path):
-                raise ValueError("This db does not exist.")
+        db_file_path = self.save_path + "/" + self.filename + ".db"
 
-        study = optuna.load_study(
-            study_name=self.filename,
-            sampler=self.choose_sampler(seed=seed),
-            pruner=self.choose_pruner(),
-            storage=self.storage,
-        )
-
-<<<<<<< HEAD
-        return study
-
-    def _run_study(self, objective, device="cuda:0", forced_run=True):
-=======
-        if os.path.exists(db_file_path) or self.sql_name == 'mysql':
-            study = optuna.load_study(study_name = self.filename,
-                                      sampler = self.choose_sampler(seed = seed),
-                                      pruner = self.choose_pruner(),
-                                      storage = self.storage)
+        if os.path.exists(db_file_path) or self.sql_name == "mysql":
+            study = optuna.load_study(
+                study_name=self.filename,
+                sampler=self.choose_sampler(seed=seed),
+                pruner=self.choose_pruner(),
+                storage=self.storage,
+            )
 
         else:
-            raise ValueError('This db does not exist.')
+            raise ValueError("This db does not exist.")
         return study
 
-    def _run_study(self, objective, device = 'cpu', seed = 42, forced_run = True):
+    def _run_study(self, objective, device="cpu", seed=42, forced_run=True):
         """
-        
+
         2023.4.11 佐々木
         multi-threadによる計算よりもmultiprocessingの方が、CUDAでの計算が早いことがわかった。
-        
+
         しかし、すでに、以下のバグが発生していることがわかっている。
-        
+
         ・初期値がrandomのとき、GridSamplerを使うと同じepsの値が異なるworkerで計算されてしまう(しかし、初期値の乱数は異なる)。
           → これは2023.4.10に作成した "if sampler_name == grid" で起こっているバグ。おそらくoptunaの仕様かもしれない。
-        
+
         ・初期値がrandomのとき、multiprocessingを使うと、各workerの1回目のとき、tqdmが表示されない。2回目以降は表示される。
           おそらくjupyter側のバグかと思われる。
-        
+
         ・multiprocessingで、GridSamplerを使うと、一つのepsの値だけ、重複して計算されてしまう。
-        
+
         2023.4.12 佐々木
         ・grid searchの問題はoptunaの仕様らしい
         ・tqdmのバグは、各processごとにprintを噛ませて回避できた。
-        
+
         2024.4.18 佐々木
         parallelでの計算は全てコメントアウトにした。
         """
-        
->>>>>>> 732d91eba67f6d58e7c5ccb6395b4259c14f060d
+
         if self.delete_study:
             self._confirm_delete()
 
         if forced_run:
-<<<<<<< HEAD
             self.create_study()  # dbファイルがない場合、ここでloadをさせないとmulti_runが正しく動かなくなってしまう。
 
-            if self.sampler_name == "grid":
-                # 2023.4.10 佐々木
-                # grid searchの場合、epsの値は決まっているので、通常の書き方で問題なし。
+            def _local_runner(objective, num_trials, device, seed, worker_id=0):
                 tt = functools.partial(objective, device=device)
-                study = self.load_study()
-                study.optimize(tt, n_trials=self.num_trial, n_jobs=self.n_jobs)
+                study = self.load_study(seed=seed + worker_id)
+                study.optimize(tt, n_trials=num_trials)
 
-            else:
-                # 2023.4.10 佐々木
-                # grid search以外は、以下のように書かないと乱数が固定されない問題がある(本当に面倒くさい・・・)。
-                def multi_run(objective, seed, num_trials, device):
-                    tt = functools.partial(objective, device=device)
-                    study = self.load_study(seed=seed)
-                    study.optimize(tt, n_trials=num_trials, n_jobs=1)
-
-                seed = 42
-
-                with ThreadPoolExecutor(self.n_jobs) as pool:
-                    for i in range(self.n_jobs):
-                        if device == "multi":
-                            device = "cuda:" + str(i % 4)
-                        elif "cuda" in device:
-                            device = device
-                        elif device == "cpu":
-                            device = "cpu"
-
-                        pool.submit(multi_run, objective, seed + i, self.num_trial // self.n_jobs, device)
-
-=======
-            self.create_study() #dbファイルがない場合、ここでloadをさせないとmulti_runが正しく動かなくなってしまう。
-            
-            def _local_runner(objective, num_trials, device, seed, worker_id = 0):
-                tt = functools.partial(objective, device = device)
-                study = self.load_study(seed = seed + worker_id)
-                study.optimize(tt, n_trials = num_trials)
-                
-            if self.sampler_name.lower() == 'tpe':
-                assert self.n_jobs == 1, 'TPE-Sampler does not work in a proper way if n_jobs > 1.'
+            if self.sampler_name.lower() == "tpe":
+                assert self.n_jobs == 1, "TPE-Sampler does not work in a proper way if n_jobs > 1."
                 _local_runner(objective, self.num_trial, device, seed)
-            
-            elif self.sampler_name.lower() == 'random' or self.sampler_name.lower() == 'grid':
+
+            elif self.sampler_name.lower() == "random" or self.sampler_name.lower() == "grid":
                 if self.n_jobs == 1:
                     _local_runner(objective, self.num_trial, device, seed)
-                
+
                 elif self.n_jobs == -1:
-                    raise ValueError("Do not use n_jobs = -1 in this library, please use 'os.cpu_count()' instead of it.")
-                
+                    raise ValueError(
+                        "Do not use n_jobs = -1 in this library, please use 'os.cpu_count()' instead of it."
+                    )
+
                 elif self.n_jobs > 1:
-                    if self.to_types == 'numpy':
-                        warnings.warn('parallel computation may be slower than single computation for numpy...', UserWarning)
+                    if self.to_types == "numpy":
+                        warnings.warn(
+                            "parallel computation may be slower than single computation for numpy...", UserWarning
+                        )
                     worker_arr = np.array_split(np.arange(self.num_trial), self.n_jobs)
 
                     with ThreadPoolExecutor(self.n_jobs) as pool:
                         for i in range(self.n_jobs):
-                            if device == 'multi':
-                                device = 'cuda:' + str(i % 4)
-                            
+                            if device == "multi":
+                                device = "cuda:" + str(i % 4)
+
                             worker_trial = len(worker_arr[i])
-                            pool.submit(_local_runner, objective, worker_trial, device, seed, worker_id = i)
-            
->>>>>>> 732d91eba67f6d58e7c5ccb6395b4259c14f060d
+                            pool.submit(_local_runner, objective, worker_trial, device, seed, worker_id=i)
+
         study = self.load_study()
 
         return study
@@ -328,7 +272,7 @@ class RunOptuna:
 
         objective = functools.partial(objective, **kwargs)
 
-        study = self._run_study(objective, device = device, forced_run = forced_run)
+        study = self._run_study(objective, device=device, forced_run=forced_run)
 
         return study
 
@@ -340,13 +284,8 @@ class RunOptuna:
         if self.sampler_name == "random":
             sampler = optuna.samplers.RandomSampler(seed)
 
-<<<<<<< HEAD
         elif self.sampler_name == "grid":
-            sampler = optuna.samplers.GridSampler(self.search_space)
-=======
-        elif self.sampler_name == 'grid':
-            sampler = optuna.samplers.GridSampler(self.search_space, seed = seed)
->>>>>>> 732d91eba67f6d58e7c5ccb6395b4259c14f060d
+            sampler = optuna.samplers.GridSampler(self.search_space, seed=seed)
 
         elif self.sampler_name.lower() == "tpe":
             sampler = optuna.samplers.TPESampler(
