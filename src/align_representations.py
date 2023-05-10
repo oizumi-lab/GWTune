5d11f4b0b9d81aabf3b7d01d988cf51f8fac077c
KenTakeda <tkkentakeda1248@g.ecc.u-tokyo.ac.jp>
2023-05-10 10:07:26 +0900

    minor change

--- src/align_representations.py
+++ src/align_representations.py
@@ -287,7 +287,7 @@ class Align_Representations:
         plt.tick_params(axis = "both", which = "major")
         plt.subplots_adjust(left=0.2, right=0.9, bottom = 0.2)
         if fig_dir is not None:
-            plt.savefig(os.path.join(fig_dir, f"Accuracy_{eval_type}.png"))
+            plt.savefig(os.path.join(fig_dir, f"Accuracy_{eval_type}_{self.config.data_name}.png"))
         plt.show()
     
     def visualize_embedding(self, dim = 3, color_labels = None, category_name_list = None, category_num_list = None, category_idx_list = None, fig_dir = None):
