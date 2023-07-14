d73bda6f8c60c8fc62f917b67f9cba447c1b6884
MSA8D8 <demo.abc2@gmail.com>
2023-07-14 11:18:29 +0900

    初期値の修正。

--- src/utils/init_matrix.py
+++ src/utils/init_matrix.py
@@ -32,25 +32,11 @@ class InitMatrix():
         Returns:
             _type_: _description_
         """
-        
 
         np.random.seed(seed) # fix the seed of numpy.random, seed can be changed by user.
         
         if initialize == 'random':
             T = self.make_random_init_plan()
-
-        # elif initialize == 'permutation':
-        #     ts = np.zeros(self.matrix_size)
-        #     ts[0] = 1 / self.matrix_size
-        #     T = self.initialize_matrix(ts=ts)
-
-        # elif initialize == 'any_permutation':
-        #     T = self.initialize_matrix()
-
-        # elif initialize == 'beta':
-        #     ts = np.random.beta(2, 5, self.matrix_size)
-        #     ts = ts / (self.matrix_size * np.sum(ts))
-        #     T = self.initialize_matrix(ts=ts)
 
         elif initialize == 'uniform':
             T = np.outer(ot.unif(self.source_size), ot.unif(self.target_size))
@@ -63,49 +49,24 @@ class InitMatrix():
 
         return T
 
-    def randOrderedMatrix(self): #ここも間違っているかもしれないです(2023/7/6)
-        """
-        各行・各列に重複なしに[0,n]のindexを持つmatrixを作成
-        Parameters
-            n : int 行列のサイズ
-        Returns
-            np.ndarray 重複なしのindexを要素に持つmatrix
-        """
-        matrix = np.zeros((self.source_size, self.target_size))
-        rows = np.tile(np.arange(0, self.matrix_size), 2)
-
-        for i in range(self.matrix_size):
-            matrix[i, :] = rows[i : i + self.matrix_size]
-
-        r = np.random.choice(self.source_size, self.target_size, replace=False)
-        c = np.random.choice(self.source_size, self.target_size, replace=False)
-        matrix = matrix[r, :]
-        matrix = matrix[:, c]
-        return matrix.astype(int)
-
-    def initialize_matrix(self, ts=None):
-        matrix = self.randOrderedMatrix()
-        if ts is None:
-            ts = np.random.uniform(0, 1, self.matrix_size)
-            ts = ts / (self.matrix_size * np.sum(ts))
-
-        T = np.array([ts[idx] for idx in matrix])
-        return T
-
     def make_random_init_plan(self):
         """
         make random initial matrix.
         """
         T = np.random.rand(self.source_size, self.target_size) # create a random matrix
+        
         rep = 100 # number of repetitions
+        
         for _ in range(rep):
             # normalize each row so that the sum is 1
             p = T.sum(axis=1, keepdims=True)
             T = T / p
+            
             # normalize each column so that the sum is 1
             q = T.sum(axis=0, keepdims=True)
             T = T / q
-        T = T / self.source_size #ここ間違っているかもしれないです・・・ 
+        
+        T = T / T.sum() #2023.7.14 fixed by Ohizumi
         return T
 
 # %%
