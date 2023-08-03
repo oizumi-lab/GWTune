7638f5c319eb07e1d1b4d82b0f7cfa70bdab6fef
MSA8D8 <demo.abc2@gmail.com>
2023-08-03 13:46:09 +0900

    論文に使うDNNのfigureができた。

--- src/utils/visualize_functions.py
+++ src/utils/visualize_functions.py
@@ -267,6 +267,8 @@ class VisualizeEmbedding():
         markers_list = kwargs.get('markers_list', None)
         marker_size = kwargs.get('marker_size', 30)
         cmap = kwargs.get('cmap', "viridis")
+        
+        show_figure = kwargs.get('show_figure', True)
 
         if color_labels is None:
             if self.num_category_list is None:
@@ -362,8 +364,12 @@ class VisualizeEmbedding():
             cbar.set_label(colorbar_label, size=xlabel_size)
             cbar.ax.tick_params(labelsize=xlabel_size)
             cbar.mappable.set_clim(colorbar_range[0], colorbar_range[1])
-
+        
         if save_dir is not None:
             plt.savefig(save_dir)
 
-        plt.show()
+        if show_figure:
+            plt.show()
+        
+        plt.clf()
+        plt.close()
